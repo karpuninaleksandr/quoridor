51baaa6e2425a70109d19e06543227f1c2ffe274
aleksandrkarpunin <aleksandrkarpunin0889@gmail.com>
2026-03-05 17:50:46 +0300

    упрощение логики GameProcessor через взаимодействие с модельными перечислениями напрямую

--- src/main/java/ru/ac/uniyar/service/GameProcessor.java
+++ src/main/java/ru/ac/uniyar/service/GameProcessor.java
@@ -3,33 +3,16 @@ package ru.ac.uniyar.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.ac.uniyar.model.Game;
-import ru.ac.uniyar.model.enums.ComputerAlgorithmType;
-import ru.ac.uniyar.model.enums.ComputerPlayerHardnessLevel;
 import ru.ac.uniyar.model.enums.GameSize;
 import ru.ac.uniyar.model.players.ComputerPlayerFabric;
 import ru.ac.uniyar.model.players.HumanPlayer;
 import ru.ac.uniyar.model.players.Player;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Service
 public class GameProcessor {
     @Autowired
     private ComputerPlayerFabric computerPlayerFabric;
     private Game game;
-
-    public List<String> getAvailableBoardSizes() {
-        return Arrays.stream(GameSize.values()).map(GameSize::getDescription).toList();
-    }
-
-    public List<String> getAvailablePlayerTypes() {
-        return Arrays.stream(ComputerAlgorithmType.values()).map(ComputerAlgorithmType::getDescription).toList();
-    }
-
-    public List<String> getAvailableHardnessLevels() {
-        return Arrays.stream(ComputerPlayerHardnessLevel.values()).map(ComputerPlayerHardnessLevel::getDescription).toList();
-    }
 
     public void startNewGame(String sizeDescription, String typeOfPlayer1, String typeOfPlayer2, String gameHardness) {
         GameSize gameSize = GameSize.findByDescription(sizeDescription);
--- src/main/java/ru/ac/uniyar/ui/StartPageController.java
+++ src/main/java/ru/ac/uniyar/ui/StartPageController.java
@@ -1,5 +1,8 @@
 package ru.ac.uniyar.ui;
 
+import ru.ac.uniyar.model.enums.ComputerAlgorithmType;
+import ru.ac.uniyar.model.enums.ComputerPlayerHardnessLevel;
+import ru.ac.uniyar.model.enums.GameSize;
 import ru.ac.uniyar.service.GameProcessor;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.ComboBox;
@@ -9,11 +12,11 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Route("/start")
 public class StartPageController extends VerticalLayout {
-
     public StartPageController(GameProcessor gameProcessor) {
         setSizeFull();
         setAlignItems(Alignment.CENTER);
@@ -22,20 +25,20 @@ public class StartPageController extends VerticalLayout {
         H1 title = new H1("Игра «Коридор»");
 
         ComboBox<String> sizeSelector = new ComboBox<>("Размер поля");
-        sizeSelector.setItems(gameProcessor.getAvailableBoardSizes());
+        sizeSelector.setItems(Arrays.stream(GameSize.values()).map(GameSize::getDescription).toList());
         sizeSelector.setPlaceholder("Выберите размер");
 
         ComboBox<String> player1 = new ComboBox<>("Выберите первого игрока");
         List<String> player1s = new ArrayList<>();
         player1s.add("Игрок");
-        player1s.addAll(gameProcessor.getAvailablePlayerTypes());
+        player1s.addAll(Arrays.stream(ComputerAlgorithmType.values()).map(ComputerAlgorithmType::getDescription).toList());
         player1.setItems(player1s);
 
         ComboBox<String> player2 = new ComboBox<>("Выберите второго игрока");
-        player2.setItems(gameProcessor.getAvailablePlayerTypes());
+        player2.setItems(Arrays.stream(ComputerAlgorithmType.values()).map(ComputerAlgorithmType::getDescription).toList());
 
         ComboBox<String> hardness = new ComboBox<>("Выберите уровень сложности игры");
-        hardness.setItems(gameProcessor.getAvailableHardnessLevels());
+        hardness.setItems(Arrays.stream(ComputerPlayerHardnessLevel.values()).map(ComputerPlayerHardnessLevel::getDescription).toList());
 
         Button startButton = new Button("Начать игру", event -> {
             String size = sizeSelector.getValue();
