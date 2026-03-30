c23e375f0954bc7984f7d0f4e178f4ee700addfe
aleksandrkarpunin <aleksandrkarpunin0889@gmail.com>
2026-03-30 13:38:49 +0300

    фикс минимакса

--- src/main/java/ru/ac/uniyar/model/algorithms/MinimaxAlgorithm.java
+++ src/main/java/ru/ac/uniyar/model/algorithms/MinimaxAlgorithm.java
@@ -5,6 +5,7 @@ import ru.ac.uniyar.model.Move;
 import ru.ac.uniyar.model.enums.ComputerAlgorithmType;
 import ru.ac.uniyar.model.enums.ComputerPlayerHardnessLevel;
 import ru.ac.uniyar.model.enums.MoveType;
+import ru.ac.uniyar.model.enums.GameSize;
 
 import java.util.*;
 
@@ -16,45 +17,76 @@ public class MinimaxAlgorithm implements Algorithm {
 
     @Override
     public Move getMove(Board board, ComputerPlayerHardnessLevel hardnessLevel, int playerId, int amountOfWallsLeft) {
-        int depth = switch (hardnessLevel) {
+        int size = (int) Math.sqrt(board.getTiles().size());
+
+        long timeLimit = switch (hardnessLevel) {
+            case EASY -> 1000L * (size / GameSize.NORMAL.getAmountOfTilesPerSide());
+            case MEDIUM -> 2000L * (size / GameSize.NORMAL.getAmountOfTilesPerSide());
+            case HARD -> 3000L * (size / GameSize.NORMAL.getAmountOfTilesPerSide());
+        };
+
+        int maxDepth = switch (hardnessLevel) {
             case EASY -> 2;
             case MEDIUM -> 4;
             case HARD -> 10;
         };
 
-        int size = (int) Math.sqrt(board.getTiles().size());
+        long endTime = System.currentTimeMillis() + timeLimit;
+        Move bestMove = null;
 
-        List<Move> moves = getMoves(board, playerId, amountOfWallsLeft);
+        for (int depth = 1; depth <= maxDepth; depth++) {
+            if (System.currentTimeMillis() > endTime) break;
+            try {
+                Move move = search(board, depth, playerId, size, amountOfWallsLeft, amountOfWallsLeft, endTime);
+                if (move != null) bestMove = move;
+            } catch (RuntimeException e) {
+                break;
+            }
+        }
+
+        return bestMove;
+    }
+
+    private Move search(Board board, int depth, int playerId, int size, int walls1, int walls2, long endTime) {
+        List<Move> moves = getMoves(board, playerId, walls1);
 
         Move bestMove = null;
         int bestValue = Integer.MIN_VALUE;
 
         for (Move move : moves) {
+            checkTime(endTime);
+
             Board copy = board.copy();
             applyMove(copy, move);
 
-            int nextWalls = amountOfWallsLeft;
+            int nextWalls1 = walls1;
+            int nextWalls2 = walls2;
+
             if (move.getMoveType() == MoveType.PLACE_WALL) {
-                nextWalls--;
+                if (playerId == 1) nextWalls1--;
+                else nextWalls2--;
             }
 
-            int eval = minimax(copy, depth - 1, false, playerId, size, amountOfWallsLeft, nextWalls);
+            int eval = minimax(copy, depth - 1, false, playerId, size, nextWalls1, nextWalls2, endTime);
 
             if (eval > bestValue) {
                 bestValue = eval;
                 bestMove = move;
             }
         }
+
         return bestMove;
     }
 
-    private int minimax(Board board, int depth, boolean maximizing, int playerId, int size, int wallsLeft1, int wallsLeft2) {
+    private int minimax(Board board, int depth, boolean maximizing, int playerId, int size, int walls1, int walls2, long endTime) {
+        checkTime(endTime);
+
         if (depth == 0) {
-            return evaluate(board, playerId, size, wallsLeft1, wallsLeft2);
+            return evaluate(board, playerId, size, walls1, walls2);
         }
 
         int currentPlayer = maximizing ? playerId : (3 - playerId);
-        int currentWalls = currentPlayer == 1 ? wallsLeft1 : wallsLeft2;
+        int currentWalls = currentPlayer == 1 ? walls1 : walls2;
 
         List<Move> moves = getMoves(board, currentPlayer, currentWalls);
 
@@ -62,38 +94,48 @@ public class MinimaxAlgorithm implements Algorithm {
             int maxEval = Integer.MIN_VALUE;
 
             for (Move move : moves) {
+                checkTime(endTime);
+
                 Board copy = board.copy();
                 applyMove(copy, move);
 
-                int nextWalls1 = wallsLeft1;
-                int nextWalls2 = wallsLeft2;
+                int nextWalls1 = walls1;
+                int nextWalls2 = walls2;
 
                 if (move.getMoveType() == MoveType.PLACE_WALL) {
                     if (currentPlayer == 1) nextWalls1--;
                     else nextWalls2--;
                 }
 
-                maxEval = Math.max(maxEval, minimax(copy, depth - 1, false, playerId, size, nextWalls1, nextWalls2));
+                maxEval = Math.max(maxEval, minimax(copy, depth - 1, false, playerId, size, nextWalls1, nextWalls2, endTime));
             }
             return maxEval;
         } else {
             int minEval = Integer.MAX_VALUE;
 
             for (Move move : moves) {
+                checkTime(endTime);
+
                 Board copy = board.copy();
                 applyMove(copy, move);
 
-                int nextWalls1 = wallsLeft1;
-                int nextWalls2 = wallsLeft2;
+                int nextWalls1 = walls1;
+                int nextWalls2 = walls2;
 
                 if (move.getMoveType() == MoveType.PLACE_WALL) {
                     if (currentPlayer == 1) nextWalls1--;
                     else nextWalls2--;
                 }
 
-                minEval = Math.min(minEval, minimax(copy, depth - 1, true, playerId, size, nextWalls1, nextWalls2));
+                minEval = Math.min(minEval, minimax(copy, depth - 1, true, playerId, size, nextWalls1, nextWalls2, endTime));
             }
             return minEval;
+        }
+    }
+
+    private void checkTime(long endTime) {
+        if (System.currentTimeMillis() > endTime) {
+            throw new RuntimeException("timeout");
         }
     }
 }
